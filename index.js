80d0105a7164b03ed9c99c1890a82076030a30f6
asifzulfiqar8 <asifzulfiqar43@gmail.com>
2025-02-26 19:35:17 +0500

    code updated

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-let let1 = 0;
-let let2 = 1;
+// JS closures concept. Closures allow a function to remember the variables from its parent scope even after the parent function has finished executing.
 
-for (let i = 0; i < 18; i++) {}
+const counter = () => {
+  let count = 0;
+
+  return {
+    increament: () => {},
+    decrement: () => {},
+    increament: () => {},
+  };
+};
